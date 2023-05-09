a43a1bbc9fd7913cd8cf597d8876112eda030a58
RIMRIM <96475505+li-moo@users.noreply.github.com>
2023-05-09 10:10:22 +0900

    init

---
+++ fe_ipb_page/src/layouts/FullLayout.js
@@ -0,0 +1,31 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { Container } from "reactstrap";
+import HeaderTop from "./HeaderTop";
+
+const FullLayout = () => {
+  return (
+    <main>
+      <div className="pageWrapper d-lg-flex">
+        {/********Sidebar**********/}
+        <aside className="sidebarArea shadow" id="sidebarArea">
+          <Sidebar />
+        </aside>
+        {/********Content Area**********/}
+
+        <div className="contentArea">
+          {/********header**********/}
+          <HeaderTop />
+          <Header />
+          {/********Middle Content**********/}
+          <Container className="p-4 wrapper" fluid>
+            <Outlet />
+          </Container>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default FullLayout;
---
+++ fe_ipb_page/src/layouts/Header.js
@@ -0,0 +1,151 @@
+import React from "react";
+import {
+  Navbar,
+  Collapse,
+  Nav,
+  NavItem,
+  NavbarBrand,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Dropdown,
+  Button,
+} from "reactstrap";
+import { ReactComponent as LogoWhite } from "../assets/images/logos/xtremelogowhite.svg";
+import user1 from "../assets/images/users/user1.jpg";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  // const [dropdownOpen, setDropdownOpen] = React.useState(false);
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+  const Handletoggle = () => {
+    // setIsOpen(!isOpen);
+  };
+  const showMobilemenu = () => {
+    // document.getElementById("sidebarArea").classList.toggle("showSidebar");
+  };
+  return (
+    <Navbar color="dark" dark expand="md">
+      <div className="d-flex align-items-center">
+        <NavbarBrand href="/" className="d-lg-none">
+          {/* <LogoWhite /> */}
+        </NavbarBrand>
+        {/* <Button
+          color="dark"
+          className="d-lg-none"
+          onClick={() => showMobilemenu()}
+        >
+          <i className="bi bi-list"></i>
+        </Button> */}
+      </div>
+      <div className="hstack gap-2">
+        <Button
+          color="dark"
+          size="sm"
+          className="d-sm-block d-md-none"
+          onClick={Handletoggle}
+        >
+          {isOpen ? (
+            <i className="bi bi-x"></i>
+          ) : (
+            <i className="bi bi-three-dots-vertical"></i>
+          )}
+        </Button>
+      </div>
+
+      <Collapse navbar isOpen={isOpen}>
+      {/* <Collapse> */}
+        <Nav className="me-auto" navbar>
+          {/* <NavItem>
+            <Link to="/starter" className="nav-link">
+              Starter
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </NavItem> */}
+
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              상품 관리
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              발주
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              매출
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              배송
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              이벤트
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <UncontrolledDropdown inNavbar nav>
+            <DropdownToggle caret nav>
+              게시판
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
