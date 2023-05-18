2f8b8d9ed914f170a2f00ac757c4d6e73b2a40f7
AnzioR <zkxmtpdl@gmail.com>
2023-05-18 15:07:57 +0900

    remove ordercart

--- fe_ipb_page/src/components/pages/storeproduct/StoreProductList.jsx
+++ fe_ipb_page/src/components/pages/storeproduct/StoreProductList.jsx
@@ -60,6 +60,7 @@ function StoreProductList() {
     {
       title: '이름',
       dataIndex: 'product_name',
+      sorter: (a, b) => a.product_name.localeCompare(b.product_name),
       render: (text, record) => (
         <Link to={`/product/detail/${record.id}`} key={record.id}>{text}</Link>
       ),
@@ -77,6 +78,7 @@ function StoreProductList() {
     {
       title: '유통기한',
       dataIndex: 'exp',
+      sorter: (a, b) => a.exp.localeCompare(b.exp),
     },
   ];
 
