b7bddfe75c07270abc85622371ec599ba8386f88
RIMRIM <96475505+li-moo@users.noreply.github.com>
2023-06-02 20:28:27 +0900

    FEAT-low

--- fe_ipb_page/src/components/sse/DropDown.jsx
+++ fe_ipb_page/src/components/sse/DropDown.jsx
@@ -106,13 +106,29 @@ import PropTypes from 'prop-types';
 import { HiOutlineBell } from 'react-icons/hi';
 import { logInState } from '../state/loginState';
 import { useRecoilState } from 'recoil';
+import { alarmState } from '../state/alarmState';
+import styles from './DropDown.module.css'
 
 function DropDown({ direction, ...args }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [loginData, setLoginData] = useRecoilState(logInState);
   const [messages, setMessages] = useState([]);
+  const [alarmData, setAlarmData] = useRecoilState(alarmState);
 
-  const toggle = () => setDropdownOpen((prevState) => !prevState);
+  // const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  const toggle = () => {
+    setDropdownOpen((prevState) => !prevState);
+    if (!dropdownOpen) {
+      setAlarmData(0);
+    }
+  };
+
+  const url = `http://localhost:8080/notifications/expiration/${loginData.store_id}`;
+
+  useEffect(() => {
+    fetchSSE();
+  }, []);
 
   // useEffect(() => {
   //   const eventSource = new EventSource(`http://localhost:8080/notifications/expiration?store_id=${loginData.store_id}`, {
@@ -137,21 +153,93 @@ function DropDown({ direction, ...args }) {
   //   };
   // }, [loginData.store_id]);
 
+  const fetchSSE = () => {
+    const eventSource = new EventSource(url, {
+      headers: {
+        Accept: 'text/event-stream',
+      }
+    });
+
+    // eventSource.onopen = (e) => {
+    //   console.log("1. 서버와 연결되셨습니다.");
+      
+    // };\
+    eventSource.onopen = function(event) {
+      if (eventSource.readyState === EventSource.OPEN) {
+        console.log('연결 성공');
+      } else {
+        console.log('연결 실패');
+      }
+    };
+    
+    eventSource.onmessage = (e) => {      
+      console.log(JSON.parse(e.data)[1].data);
+      const onmessageData = JSON.parse(e.data)[1].data
+      // setMessages((prev) => [...prev, JSON.parse(e.data)[1].data]);
+      setMessages((prev) => [...prev, onmessageData]);
+      setAlarmData(onmessageData.length)
+    };
+
+    eventSource.onerror = (e) => {
+      // 종료 또는 에러 발생 시 할 일
+      eventSource.close();
+
+      if (e.error) {
+        console.log("에러가 발생했습니다.");
+        console.log(e);
+      }
+
+      if (e.target.readyState === EventSource.CLOSED) {
+        // 종료 시 할 일
+      }
+      return () => {
+        eventSource.close(); //  SSE 연결 종료
+      };
+    };
+  };
+
   return (
+    // <div id='top-myDrop'>
+    //   <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={direction} id='top-myDrop--i'>
+    //     <DropdownToggle id='top-myDrop--ii' style={{ border: 'none', backgroundColor: '#2962FF', color: 'white' }}>
+    //       <HiOutlineBell />
+    //       {/* <p> {cartData && cartData.map((item) => (</p> */}
+    //       {
+    //         alarmData && (
+    //           <p className={styles.alarmRed}></p>
+    //           )
+    //       }
+    //       {
+    //         dropdownOpen && (
+    //           setAlarmData === null
+    //         )
+    //       }
+    //     </DropdownToggle>
+    //     <DropdownMenu {...args}>
+    //       <DropdownItem divider />
+    //       <DropdownItem>{alarmData}</DropdownItem>
+    //       {messages.map((message, index) => (
+    //         <DropdownItem key={index}>{message}</DropdownItem>
+    //       ))}
+    //     </DropdownMenu>
+    //   </Dropdown>
+    // </div>
     <div id='top-myDrop'>
-      <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={direction} id='top-myDrop--i'>
-        <DropdownToggle id='top-myDrop--ii' style={{ border: 'none', backgroundColor: '#2962FF', color: 'white' }}>
-          <HiOutlineBell />
-        </DropdownToggle>
-        <DropdownMenu {...args}>
-          <DropdownItem divider />
-          <DropdownItem>메세지1</DropdownItem>
-          {messages.map((message, index) => (
-            <DropdownItem key={index}>{message}</DropdownItem>
-          ))}
-        </DropdownMenu>
-      </Dropdown>
-    </div>
+    <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={direction} id='top-myDrop--i'>
+      <DropdownToggle id='top-myDrop--ii' style={{ border: 'none', backgroundColor: '#2962FF', color: 'white' }}>
+        <HiOutlineBell />
+        {alarmData > 0 && <p className={styles.alarmRed}></p>}
+        {/* {!dropdownOpen && alarmData === 0 && <p className={styles.alarmblack}></p>} */}
+      </DropdownToggle>
+      <DropdownMenu {...args}>
+        <DropdownItem divider />
+        <DropdownItem>{alarmData}</DropdownItem>
+        {messages.map((message, index) => (
+          <DropdownItem key={index}>{message}</DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  </div>
   );
 }
 
--- fe_ipb_page/src/components/sse/SSETest.jsx
+++ fe_ipb_page/src/components/sse/SSETest.jsx
@@ -6,6 +6,7 @@ import { EventSourcePolyfill } from 'event-source-polyfill';
 
 function SSETest() {
   const [messages, setMessages] = useState([]);
+  const [messagesLowItem, setMessagesLowItem] = useState([]);
   const [loginData, setLoginData] = useRecoilState(logInState);
 
   console.log("SSETEst>> 로그인데이타:", loginData?.store_id);
@@ -14,16 +15,16 @@ function SSETest() {
   const url = `http://localhost:8080/notifications/expiration/${loginData.store_id}`;
 
   useEffect(() => {
-    fetchSSE();
+    fetchEXPSSE();
+    fetchLowItemSSE();
   }, []);
 
-  const fetchSSE = () => {
+  const fetchEXPSSE = () => {
     const eventSource = new EventSource(url, {
       headers: {
         Accept: 'text/event-stream',
       }
     });
-
     // eventSource.onopen = (e) => {
     //   console.log("1. 서버와 연결되셨습니다.");
       
@@ -61,6 +62,48 @@ function SSETest() {
     };
   };
 
+  const fetchLowItemSSE = () => {
+    const LowItem_url = `http://localhost:8080/notifications/low-inventory/${loginData.store_id}`;
+
+    const eventSource = new EventSource(LowItem_url, {
+      headers: {
+        Accept: 'text/event-stream',
+      }
+    });
+
+    eventSource.onopen = function(event) {
+      if (eventSource.readyState === EventSource.OPEN) {
+        console.log('연결 성공');
+      } else {
+        console.log('연결 실패');
+      }
+    };
+    
+    eventSource.onmessage = (e) => {      
+      console.log(JSON.parse(e.data)[1].data);
+      const onmessageData = JSON.parse(e.data)[1].data
+      // setMessages((prev) => [...prev, JSON.parse(e.data)[1].data]);
+      setMessagesLowItem((prev) => [...prev, onmessageData]);
+    };
+
+    eventSource.onerror = (e) => {
+      // 종료 또는 에러 발생 시 할 일
+      eventSource.close();
+
+      if (e.error) {
+        console.log("에러가 발생했습니다.");
+        console.log(e);
+      }
+
+      if (e.target.readyState === EventSource.CLOSED) {
+        // 종료 시 할 일
+      }
+      return () => {
+        eventSource.close(); //  SSE 연결 종료
+      };
+    };
+  };
+
   return (
     <>
       <div>
@@ -70,6 +113,11 @@ function SSETest() {
           <h1>Server-Sent Events</h1>
           <ul>
             {messages.map((message, index) => (
+              <li key={index}>{message}</li>
+            ))}
+          </ul>
+          <ul>
+            {messagesLowItem.map((message, index) => (
               <li key={index}>{message}</li>
             ))}
           </ul>
